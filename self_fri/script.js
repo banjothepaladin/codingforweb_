0cd403cccfbf11eb192cdf8fe2f58df07845d660
banjothepaladin <141610655+banjothepaladin@users.noreply.github.com>
2023-09-01 16:02:54 +0530

    Add files via upload

---
+++ self_fri/script.js
@@ -0,0 +1,261 @@
+let fontzdata = [
+    {
+      id: "Chivo",
+      name: "Chivo",
+      demo: "1234567890 <br>!@#$%^&*()<br>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br>abcdefghijklmnopqrstuvwxyz<br>TyPeScRiPt®",
+      path: "./fontfolder/Chivo-Italic-VariableFont_wght",
+      cat: "SIMPLE"
+    },
+    {
+      id: "Clash Display",
+      name: "Clash Display",
+      demo: "1234567890 <br>!@#$%^&*()<br>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br>abcdefghijklmnopqrstuvwxyz<br>TyPeScRiPt®",
+      path: "./fontfolder/ClashDisplay-Variable",
+      cat: "CURSIVE"
+    },
+    {
+      id: "Comfortaa",
+      name: "Comfortaa",
+      demo: "1234567890 <br>!@#$%^&*()<br>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br>abcdefghijklmnopqrstuvwxyz<br>TyPeScRiPt®",
+      path: "./fontfolder/Comfortaa-VariableFont_wght",
+      cat: "Y2K"
+    },
+    {
+      id: "Cookie Monster",
+      name: "Cookie Monster",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/CookimonstapersonaluseExtbd-mL0Vm",
+      cat: "FUNKY"
+    },
+    {
+      id: "Dots All For Now",
+      name: "Dots All For Now",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/DotsAllForNow",
+      cat: "SIMPLE"
+    },
+    {
+      id: "DX Slight",
+      name: "DX Slight",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/DxSlight-ExtBdUltraSlant- Free Personal Use",
+      cat: "CURSIVE"
+    },
+    {
+      id: "Exodus - Regular",
+      name: "Exodus - Regular",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/ExodusDemo-Regular",
+      cat: "FUNKY"
+    },
+    {
+      id: "Exodus - Sharpen",
+      name: "Exodus - Sharpen",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/ExodusDemo-Sharpen",
+      cat: "SIMPLE"
+    },
+    {
+      id: "Exodus - Shino",
+      name: "Exodus - Shino",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/ExodusDemo-Shino",
+      cat: "CURSIVE"
+    },
+    {
+      id: "Exodus - Stencil",
+      name: "Exodus - Stencil",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/ExodusDemo-Stencil",
+      cat: "Y2K"
+    },
+    {
+      id: "Exodus - Striped",
+      name: "Exodus - Striped",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/ExodusDemo-Striped",
+      cat: "FUNKY"
+    },
+    {
+      id: "Gyahegi",
+      name: "Gyahegi",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/Gyahegi",
+      cat: "SIMPLE"
+    },
+    {
+      id: "Harmony",
+      name: "Harmony",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/HARMONY Personal use",
+      cat: "CURSIVE"
+    },
+    {
+      id: "HKGrotesk",
+      name: "HKGrotesk",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/HKGrotesk-Regular",
+      cat: "Y2K"
+    },
+    {
+      id: "Leky Calgria",
+      name: "Leky Calgria",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/Leky Calgria",
+      cat: "FUNKY"
+    },
+    {
+      id: "Lexend",
+      name: "Lexend",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/Lexend-VariableFont_wght",
+      cat: "SIMPLE"
+    },
+    {
+      id: "Moglan",
+      name: "Moglan",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/Moglan_DEMO",
+      cat: "CURSIVE"
+    },
+    {
+      id: "Muller",
+      name: "Muller",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/Muller Regular",
+      cat: "Y2K"
+    },
+    {
+      id: "Neutral Face",
+      name: "Neutral Face",
+      demo: "1234567890 <br>!@#$%^&*()<br> ABCDEFGHIJKLMNOPQRSTUVWXYZ <br>abcdefghijklmnopqrstuvwxyz <br>TyPeScRiPt®",
+      path: "./fontfolder/NeutralFace",
+      cat: "FUNKY"
+    }
+];
+let fontz = document.getElementById('fontz');
+
+let generatefontz = () => {
+  return (
+  fontz.innerHTML = fontzdata.map((x) => { 
+    let {id,name,demo,path,cat} = x;  
+    return`
+    <div id=${id} class="col" style="font-family: ${name}">
+                <h3>${name}</h3>
+                <p>${demo}</p>
+            </div>
+                
+            `;})
+            .join(""));
+
+};
+
+generatefontz();
+
+
+
+
+
+
+
+
+function scrollcut() {
+  var x = document.getElementById(header);
+  x.style.height = '50vh';
+}
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function fontchange(){
+  
+  let textvalue = document.getElementById('textinput').value;
+  let para = document.getElementById('outputtxt');
+  para.innerHTML= textvalue;
+}
+
+
+
+
+let cardsItemsData = [
+  
+  ];
+
+
+
+let cards = document.getElementById("cards");
+
+let generatecards = () => {
+  
+  return (cards.innerHTML = cardsItemsData.map((x) => { 
+    let {ID,img_src,title_eng,title_hin,disc} = x;
+    return `
+ 
+            <div class="col">
+                <img src=${img_src} alt="">
+                <div class="dharti">
+                <h3>${title_eng}</h3>
+                <h3>${title_hin}</h3>
+                </div>
+                <p>
+                ${disc}
+                </p>
+            </div>
+   `
+            
+
+            
+})
+
+.join(""));
+
+};
+
+generatecards();
+
+
